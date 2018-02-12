f022dcf104e098b5393f3b1806685f9a1b84e4cf
Rune Thomas Kidmose <rtk@nfit.au.dk>
2018-02-12 14:27:49 +0100

    added{a{new{PDB{input{test

--- test/gnuplot_clash_dumb.sh
+++
@@ -1,3 +0,0 @@
-set terminal dumb 110 35
-unset xtics 
-plot "all_frames_clash.txt" using 2:xtic(1) w points pt "*" notitle
--- test/gnuplot_dumb.sh
+++
@@ -1,3 +0,0 @@
-set terminal dumb 110 35
-unset xtics
-plot "ccc_frames.txt" using 1:2 w points pt "*" notitle
